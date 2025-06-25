d3a89d5438948f6238d1598f038f660cbb7753f5
Everton <charmosapenelope595@gmail.com>
2025-06-24 21:40:22 -0300

    aprendendo o javascript

---
+++ script/mensagens.js
@@ -0,0 +1,6 @@
+// alert("O SEU SITE FOI HACKEADO PELAS FORÇAS ARMADAS AMERICANAS")
+// Criando variáveis
+// let valor = 10;
+let valor = prompt("Digite um valor:")
+// exemplo de concatenação
+alert("O valor é " + valor);
